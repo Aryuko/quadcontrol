304bd4ba6041722a8d4c44a1810d2f07ce263588
MrLarssonJr <jesper.ekvik@telia.com>
2014-10-22 08:06:44 +0200

    Tiny Update to Bridge test

--- Tester/Bridge_get_test/Bridge_get_test.ino
+++ Tester/Bridge_get_test/Bridge_get_test.ino
@@ -12,9 +12,14 @@ void loop() {
   // put your main code here, to run repeatedly:
   int bufferSize = 10;
   char myData[bufferSize];
-  Bridge.get("test", myData, bufferSize);
   
   int i;
+  for(i = 0; i < bufferSize; i++) {
+    myData[i] = (char) 0;
+  }
+  Bridge.get("test", myData, bufferSize);
+  
+  
   for(i = 0; i < bufferSize; i++) {
     Serial.print(myData[i]);
   }
