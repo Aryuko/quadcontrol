9fbd5c82128764cea93d7a966daab35540374ffb
MrLarssonJr <jesper.ekvik@telia.com>
2014-10-22 08:37:45 +0200

    Comments
    Documented servo test

--- Tester/Servo_test/Servo_test.ino
+++ Tester/Servo_test/Servo_test.ino
@@ -12,16 +12,16 @@ void setup() {
   delay(2000);  //Wait for esc to confirm startup is complete, timting should be tuned
 }
 
-int throttle = 0;
-int delta = 1;
+int throttle = 0;  //Current throttle
+int delta = 1;  //Amount the throttle shall be displaced each timestep
 
 void loop() {
   // put your main code here, to run repeatedly:
-  throttle += delta;
+  throttle += delta;  //Change the throttle by delta
   
-  if(throttle % 180 == 0) {
+  if(throttle % 180 == 0) {  //Control if delta should be reversed, it should if throttle has reached the limit of it's definition (0 <= throttle <= 180)
     delta *= -1;
   }
   
-  delay(10);
+  delay(10);  //Wait a small timestep before changing the throttle once more
 }
