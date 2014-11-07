ae3ba3c6c70f946a2cc0f13205188bd0b7acdae8
MrLarssonJr <jesper.ekvik@telia.com>
2014-11-07 14:05:53 +0100

    Added new Sensor test
    With help of libs from I2Cdev the sensor now works flawlessly.

--- Styrprogram/Styrprogram/Styrprogram.ino
+++ Styrprogram/Styrprogram/Styrprogram.ino
@@ -1,12 +1,24 @@
+//Imports to read from the sensors
+#include <Wire.h>
+
+#include <helper_3dmath.h>
+#include <MPU6050.h>
+#include <MPU6050_6Axis_MotionApps20.h>
+#include <MPU6050_9Axis_MotionApps41.h>
+
+#include <AK8975.h>
+
+
+
 //SensorValues
 int sensorACCL_X, sensorACCL_Y, sensorACCL_Z;  //Measured in g
 int sensorGYRO_X, sensorGYRO_Y, sensorGYRO_Z;  //Measuerd in rad/s
-int sensorCMPS_X, sensorCMPS_Y, sensorCMPS_Z;  //Measured in rad
+int sensorANGL_X, sensorANGL_Y, sensorANGL_Z;  //Measured in rad
 
 //TargetValues
 int targetACCL_X, targetACCL_Y, targetACCL_Z;  //Measured in g
 int targetGYRO_X, targetGYRO_Y, targetGYRO_Z;  //Measured in rad/s
-int targetCMPS_X, targetCMPS_Y, targetCMPS_Z;  //Measured in rad
+int targetANGL_X, targetANGL_Y, targetANGL_Z;  //Measured in rad
 
 //Displacment - The acceleration displacment has been smashed in the angle displacment
 int rollDisplacment, pitchDisplacment, yawDisplacment;
