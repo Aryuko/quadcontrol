a697a3532ef64b2389b232a6a2b3a6b976f3f1f6
MrLarssonJr <jesper.ekvik@telia.com>
2014-11-11 14:43:01 +0100

    Added mou to styrprogram

--- Styrprogram/Styrprogram/Styrprogram.ino
+++ Styrprogram/Styrprogram/Styrprogram.ino
@@ -1,14 +1,18 @@
-//Imports to read from the sensors
+//Imports to read from the MPU
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
 #include <Wire.h>
 
-#include <helper_3dmath.h>
-#include <MPU6050.h>
-#include <MPU6050_6Axis_MotionApps20.h>
-#include <MPU6050_9Axis_MotionApps41.h>
+//Classes for MPU
+MOU6050 mpu(0x69);
 
-#include <AK8975.h>
-
-
+//Vars for the MPU
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
 
 //SensorValues
 int sensorACCL_X, sensorACCL_Y, sensorACCL_Z;  //Measured in g
@@ -25,6 +29,7 @@ int rollDisplacment, pitchDisplacment, yawDisplacment;
 int rollPerSecondDisplacment, pitchPerSecondDisplacment, yawPerSecondDisplacment;
 
 void setup() {
+  Serial.begin(9600);
 
 }
 
@@ -38,6 +43,42 @@ void loop() {
   calcStrategy();
   
   executeStrategy();
+}
+
+void initSensors() {
+  Wire.begin();  //Start the I2C bus
+  mpu.initialize();  //Init the mpu
+
+  if(!mpu.testConnection()) {  //Test connection to the mpu 
+    Serial.println("MPU connection failed");
+  }
+  
+  devStatus = mpu.dmpInitialize();  //Init the Digital Motion Processor
+  
+  //Offsets for the MPU
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(920);
+  
+  if(devStatus == 0) {
+    mpu.setDMPEnabled(true);  //Turn on the Digital Motion Processor
+    
+    attachInterrupt(0, dmpDataReady, RISING);  //These one/two lines shouldn't do much, but included out of fear it may not work if not
+    mpuIntStatus = mpu.getIntStatus();
+    
+    dmpReady = true;  //Flag the DMP as ok
+    packetSize = mpu.dmpGetFIFOPacketSize();  //Get expected DMP packet size for later comparison
+  }
+  else {
+      // ERROR!
+      // 1 = initial memory load failed
+      // 2 = DMP configuration updates failed
+      // (if it's going to break, usually the code will be 1)
+      Serial.print(F("DMP Initialization failed (code "));
+      Serial.print(devStatus);
+      Serial.println(F(")"));
+    }
 }
 
 void retriveInput() {
