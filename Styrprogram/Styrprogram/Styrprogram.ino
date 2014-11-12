33923c8a3c0247b333f312f65e601ebe12e601e4
MrLarssonJr <jesper.ekvik@telia.com>
2014-11-12 13:28:39 +0100

    MPU now works as intended

--- Styrprogram/Styrprogram/Styrprogram.ino
+++ Styrprogram/Styrprogram/Styrprogram.ino
@@ -4,7 +4,7 @@
 #include <Wire.h>
 
 //Classes for MPU
-MOU6050 mpu(0x69);
+MPU6050 mpu(0x69);
 
 //Vars for the MPU
 bool dmpReady = false;  // set true if DMP init was successful
@@ -14,10 +14,19 @@ uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
 uint16_t fifoCount;     // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64]; // FIFO storage buffer
 
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+
 //SensorValues
-int sensorACCL_X, sensorACCL_Y, sensorACCL_Z;  //Measured in g
-int sensorGYRO_X, sensorGYRO_Y, sensorGYRO_Z;  //Measuerd in rad/s
-int sensorANGL_X, sensorANGL_Y, sensorANGL_Z;  //Measured in rad
+double sensorACCL_X, sensorACCL_Y, sensorACCL_Z;  //Measured in g
+double sensorANGL_Y, sensorANGL_P, sensorANGL_R;  //Measured in degree
 
 //TargetValues
 int targetACCL_X, targetACCL_Y, targetACCL_Z;  //Measured in g
@@ -30,7 +39,10 @@ int rollPerSecondDisplacment, pitchPerSecondDisplacment, yawPerSecondDisplacment
 
 void setup() {
   Serial.begin(9600);
-
+  
+  while(!Serial){}
+  
+  initMPU();
 }
 
 void loop() {
@@ -45,7 +57,8 @@ void loop() {
   executeStrategy();
 }
 
-void initSensors() {
+void initMPU() {
+  
   Wire.begin();  //Start the I2C bus
   mpu.initialize();  //Init the mpu
 
@@ -85,6 +98,54 @@ void retriveInput() {
 }
 
 void retriveSensorData() {
+  // if programming failed, don't try to do anything
+  if (!dmpReady) return;
+  
+  // reset interrupt flag and get INT_STATUS byte
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
+
+  // get current FIFO count
+  fifoCount = mpu.getFIFOCount();
+
+  // check for overflow (this should never happen unless our code is too inefficient)
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+      // reset so we can continue cleanly
+      mpu.resetFIFO();
+      Serial.println(F("FIFO overflow!"));
+
+  
+  }
+  else if (mpuIntStatus & 0x02) {  // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    // wait for correct available data length, should be a VERY short wait
+    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+    
+    // read a packet from FIFO
+    mpu.getFIFOBytes(fifoBuffer, packetSize);
+      
+    // track FIFO count here in case there is > 1 packet available
+    // (this lets us immediately read more without waiting for an interrupt)
+    fifoCount -= packetSize;
+
+    // display Euler angles in degrees
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+
+    // display real acceleration, adjusted to remove gravity
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetAccel(&aa, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+    
+    sensorACCL_X = aaReal.x / 16384.0;
+    sensorACCL_Y = aaReal.y / 16384.0;
+    sensorACCL_Z = aaReal.z / 16384.0;  //Measured in g
+    
+    sensorANGL_Y = ypr[0] * 180/M_PI;
+    sensorANGL_P = ypr[1] * 180/M_PI;
+    sensorANGL_R = ypr[2] * 180/M_PI;  //Measured in degree
+  }
 }
 
 void calcDisplacment() {
@@ -93,5 +154,13 @@ void calcDisplacment() {
 void calcStrategy() {
 }
 
+void jesperKrasharQuad{
+  
+}
+
 void executeStrategy() {
+}
+
+void dmpDataReady() {
+    mpuInterrupt = true;
 }
